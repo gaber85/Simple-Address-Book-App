22729f2daf09d1471d412aff24a92246b60a58b2
Gabe <gabrielr85@gmail.com>
2018-08-30 07:32:50 -0400

    Add files via upload

--- address-book-app/index.js
+++ address-book-app/index.js
@@ -1,3 +1,4 @@
+
 (function() {//Imediately Invoked Function Expression with all the code
 
     window.Contacts = window.Contacts || [];//if it exists or if not create an empty array (data structure)
@@ -26,7 +27,7 @@
 
         //injects the contact first and last name html into the DOM
 
-        document.getElementById('name-area').innerHTML += '<div class="all-contacts-container" id="name'+ maxId + '"><div class="contact-display">' + first + ' ' + last + '</div><div class="button-trash-icon tooltip" onclick="deleteContact('+maxId+')"><i class="fa fa-trash"></i><span class="tooltext">Delete Contact</span></div></div>';
+        document.getElementById('name-area').innerHTML += '<div class="all-contacts-container" id="name'+ maxId + '"><div class="contact-display" onclick="Contacts.expands('+maxId+')">' + first + ' ' + last + '</div><div class="button-trash-icon tooltip" onclick="Contacts.deleteContact('+maxId+')"><i class="fa fa-trash"></i><span class="tooltext">Delete Contact</span></div></div><div class="content" id="content'+maxId+'">First Name: '+first+'<br>Last Name: '+last+'<br>Phone: '+phone+'<br>Address: '+addr+'<span class="close">&times;</span></div>';
     }
 
     //work in progress - to edit existing contacts
@@ -38,15 +39,7 @@
         window.Contacts[idx].phone = phone;
         window.Contacts[idx].address = addr;
     }
-
-    */
-
-    window.Contacts.deleteContact = function(id) {//takes injected html function and passes the maxId on-click and hides the contact div
-        let idx = getContactsIndexFromId(id);
-        window.Contacts.splice(idx, 1);
-        document.getElementById("name" + id).style.display = "none";
-    }
-
+*/
     function getContactsIndexFromId(id) {//helper function retrieves the index of of a contact with an id number param
         for (let i = 0; i < window.Contacts.length; i++) {
             if (window.Contacts[i].id === id) {
@@ -56,7 +49,12 @@
         return -1;
     }
 
-    window.Contacts.addContact("Aaron", "Smith", "613-262-1846", "3415 Uplands Dr");//adds the first contact place holder
+    //adds a couple of contacts as place holders at first
+    window.Contacts.addContact("Aaron", "Smith", "613-262-1846", "55 Bark Terrace");
+    window.Contacts.addContact("Sam", "Brooks", "692-234-5923", "3415 Uplands Dr");
+    window.Contacts.addContact("Peter", "Smiles", "345-234-2345", "110 Meadowlands Drive");
+    window.Contacts.addContact("Arturo", "Rodriguez", "613-262-8220", "350 Brooks Street");
+    window.Contacts.addContact("Eric", "Math", "234-163-1754", "44 Carway Street");
 
     // form inputs
     let firstName = document.getElementById('firstName');
@@ -64,7 +62,7 @@
     let phoneNumber = document.getElementById('phone');
     let address = document.getElementById('address');
 
-    document.getElementById('submit-new').onclick = function() {//takes the values in the text fields and adds them to the Contacts array
+    document.getElementById('submit-new').onclick = function() {//takes the values in the text fields and adds them to the Contacts array after clicking the add button
         
         document.getElementById('theModal').style.display = "none";
         window.Contacts.addContact(
@@ -73,7 +71,7 @@
             phoneNumber.value,
             address.value
         );
-        (function(){//resets values in the form fields 
+        (function(){//resets values in the form fields (self invoked)
             if(firstName || lastName || phoneNumber || address){
                 firstName.value="";
                 lastName.value="";
@@ -83,12 +81,14 @@
         }());
     }
 
-    window.deleteContact = function(i) {//displays delete modal then on-click deletes contact (hides)
+    window.Contacts.deleteContact = function(i) {//displays delete modal then on-click deletes contact (hides)
         document.getElementById('deleteModal').style.display = "block";
         document.getElementById('delete-contactz').onclick = function() {//deletes contact
             console.log("Deleting: " + window.Contacts.getContact(i).firstname);//console logs action
             document.getElementById("name"+ i).style.display = "none";
+            document.getElementById("content"+ i).style.display = "none";
             document.getElementById('deleteModal').style.display = "none";
+            delete window.Contacts[i-1];
         }
     }
 
@@ -116,12 +116,53 @@
         }
     }
 
-    window.onclick=function(event){//clicking anywhere will close the delete prompt
+    window.onclick = function(event) {//clicking anywhere will close the delete prompt
         if (event.target == document.getElementById('deleteModal')) {
             document.getElementById('deleteModal').style.display = "none";
         }
     }
 
-    
+   window.Contacts.expands = function(i) {//on clicking a name on the list expands it and displays the contact info then clicking on it will close it
+    document.getElementById('content'+i).style.display = "block";
+    document.getElementById('content'+i).onclick = function() {
+        document.getElementById('content'+i).style.display = "none";
+    }
+   }
+
+
+    //search functionality
+
+
+    window.searchName = function() {
+        let input = document.getElementById('search-input');
+        let filter = input.value.toUpperCase();
+        for (let i = 0; i < window.Contacts.length; i++) {
+            
+            if (window.Contacts[i].firstname.toUpperCase().indexOf(filter) > -1 || window.Contacts[i].lastname.toUpperCase().indexOf(filter) > -1) {
+
+                document.getElementById('name-area').style.display = "none";
+
+                document.getElementById('search-results').innerHTML = '<div class="all-contacts-container" id="name'+ i + '"><div class="contact-display" onclick="Contacts.expands('+i+')">' + window.Contacts[i].firstname + ' ' + window.Contacts[i].lastname + '</div><div class="button-trash-icon tooltip" onclick="Contacts.deleteContact('+i+')"><i class="fa fa-trash"></i><span class="tooltext">Delete Contact</span></div></div><div class="content" id="content'+i+'">First Name: '+window.Contacts[i].firstname+'<br>Last Name: '+window.Contacts[i].lastname+'<br>Phone: '+window.Contacts[i].phone+'<br>Address: '+window.Contacts[i].address+'<span class="close">&times;</span></div>';
+
+
+            }
+
+        }
+        
+    }
+
+    window.resetNames = function() {//this will bring back all of the contacts
+        document.getElementById('search-results').style.display = "none";
+        document.getElementById('name-area').style.display = "block";
+
+        (function(){//resets values in the search field (self invoked)
+            if(document.getElementById('search-input')){
+                document.getElementById('search-input').value = "";
+            }
+        }());
+    }
+
+
+  
 }());
 
---
+++ address-book-app/jquery.js
@@ -0,0 +1,15 @@
+/*
+$(document).ready(function () {
+
+    $("#search-input").on("keyup", function (e) {
+        var input = $(this).val();
+        if (input.length > 0){
+            $('div.name-area > input').hide();
+            $( "div[id*="+input+"]" ).show();
+        }else{
+            $('#name-area < input').show();
+        }
+    });
+
+});
+*/
